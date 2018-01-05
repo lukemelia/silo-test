0ff85812fcf1d4bedfcfe04725606b35ab7a2878
Luke Melia <luke@lukemelia.com>
2018-01-04 21:57:44 -0500

    Dead code removal

--- app/transitions/cut-to-silo.js
+++
@@ -1,7 +0,0 @@
-import slideToSilo from './slide-to-silo';
-
-export default function(toSiloIndex, opts = {}) {
-  opts = Object.assign({}, opts);
-  opts.duration = 0;
-  return slideToSilo.call(this, toSiloIndex, opts);
-}
